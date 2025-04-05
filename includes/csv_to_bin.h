d71437b9ca55ea90f637d2d319c810253c18ec76
Yan <yantdd.git@gmail.com>
2025-04-05 13:10:45 -0300

    Update csv_to_bin.h

--- includes/csv_to_bin.h
+++ includes/csv_to_bin.h
@@ -2,8 +2,6 @@
 #define CSV_TO_BIN_H
 #include "parse_csv.h"
 
-
-// Outras funções acessíveis externamente
 bool escreve_registros_no_bin(FILE *csv, FILE *bin);
 void binarioNaTela(char *nomeArquivoBinario);
 
